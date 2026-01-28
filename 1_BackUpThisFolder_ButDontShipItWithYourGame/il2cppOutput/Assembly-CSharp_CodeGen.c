f3c4c5fbc194d476d06b94d6108bc68d9070cef6
Eric Domenzain M <eric.domenzain@utng.edu.mx>
2026-01-28 12:54:23 -0600

    Proyecto inicial de Realidad Virtual

---
+++ 1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,53 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void AnimateHandOnInput::Start()
+extern void AnimateHandOnInput_Start_mDFB567101BCB7DCE653AE7E320015E93D3609592 (void);
+// 0x00000002 System.Void AnimateHandOnInput::Update()
+extern void AnimateHandOnInput_Update_mC03CF4BE3804324BBC246D79B71F83B1A8848CDD (void);
+// 0x00000003 System.Void AnimateHandOnInput::.ctor()
+extern void AnimateHandOnInput__ctor_mEA43A853FB1CB874A55072BFBF7A8B78BEF60F1C (void);
+static Il2CppMethodPointer s_methodPointers[3] = 
+{
+	AnimateHandOnInput_Start_mDFB567101BCB7DCE653AE7E320015E93D3609592,
+	AnimateHandOnInput_Update_mC03CF4BE3804324BBC246D79B71F83B1A8848CDD,
+	AnimateHandOnInput__ctor_mEA43A853FB1CB874A55072BFBF7A8B78BEF60F1C,
+};
+static const int32_t s_InvokerIndices[3] = 
+{
+	5257,
+	5257,
+	5257,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	3,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
